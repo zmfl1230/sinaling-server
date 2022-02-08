bbe5117489e31cc18521d1fa8651f8740afb48ac
zmfl1230 <zmfl123097@gmail.com>
2022-02-08 14:28:26 +0900

    feat: 권한관련 에러 관리 클래스 생성

---
+++ src/main/java/com/webrtc/signalingserver/ValidatePermission.java
@@ -0,0 +1,11 @@
+package com.webrtc.signalingserver;
+public class ValidatePermission {
+
+    public static void validateLecturer(Long lecturerId, Lecture lecture) {
+        if(lecturerId == null || lecture == null || !lecturerId.equals(lecture.getLecturer().getId())) throw new IllegalArgumentException("권한이 없는 사용자입니다.");
+    }
+
+    public static void validateAccessPermission(Member student, Lecture lecture) {
+        if(student == null || lecture == null || !lecture.contains(student)) throw new IllegalArgumentException("권한이 없는 사용자입니다.");
+    }
+}
