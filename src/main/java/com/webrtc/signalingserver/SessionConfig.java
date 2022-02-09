f8d329c5e0880959808f761a4e588537db5281df
zmfl1230 <zmfl123097@gmail.com>
2022-02-09 17:37:13 +0900

    feat: [소켓 관련 서비스 함수 분리] java reflection을 이용해 의존관계 주입시 WebSocketService 내 methods set up

--- src/main/java/com/webrtc/signalingserver/SessionConfig.java
+++ src/main/java/com/webrtc/signalingserver/SessionConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -15,9 +18,26 @@ public class SessionConfig {
         return new LectureSession();
     }
 
+    @Bean
+    WebSocketService webSocketService() {
+        return new WebSocketService();
+    }
+
     @PostConstruct
     public void startLectureSession() {
         this.lectureSession().start();
+        this.lectureSession().setMethodMap(lectureSessionInit());
         log.info("소켓 서버가 시작됩니다. port: {}", this.lectureSession().getPort());
+    }
+
+    private Map<String, Method> lectureSessionInit() {
+        Map<String, Method> methodMap = new HashMap<>();
+        Method[] methods = WebSocketService.class.getMethods();
+
+        for (Method method : methods) {
+            methodMap.put(method.getName().toLowerCase(), method);
+        }
+
+        return methodMap;
     }
 }
