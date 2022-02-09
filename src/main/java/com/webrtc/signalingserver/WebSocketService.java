f1f82af691374c7e0d82076dcd8889135d4d7766
zmfl1230 <zmfl123097@gmail.com>
2022-02-09 21:05:19 +0900

    feat: [키값 암호화] 키값으로 저장되는 userId를 String 으로 변환 후, 암호화하여 키 값을 관리합니다. 한명의 사용자가 여러 세션에 접속할 수 있으니 이 userId를 키값으로 관리되고 있던 connection을 userId+lectureId 을 이용해 String 타입으로 변환 후 암호화한 키값으로 관리되도록합니다. 한 명의 사용자가 여러 강의 세션에 접근하고, 하나의 세션을 나오더라도 다른 세션을 유지하기 위함입니다.

--- src/main/java/com/webrtc/signalingserver/WebSocketService.java
+++ src/main/java/com/webrtc/signalingserver/WebSocketService.java
@@ -4,7 +4,8 @@ import com.google.gson.Gson;
 import com.webrtc.signalingserver.dto.LiveRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.java_websocket.WebSocket;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,24 +14,30 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import static com.webrtc.signalingserver.util.EncryptString.*;
+
 @Slf4j
 public class WebSocketService {
 
     @PersistenceContext
     EntityManager em;
 
+    @Autowired
+    private RedisTemplate<?, ?> redisTemplate;
+
     // member id, member socket
-    private final Map<Long, WebSocket> connections = new HashMap<>();
+//    private final ValueOperations<Long, WebSocket> con = redisTemplate.opsForValue();
+    private final Map<String, WebSocket> connections = new HashMap<>();
 
     // lecture id, session participants
-    private final Map<Long, List<Long>> sessionManager = new HashMap<>();
+    private final Map<String, List<String>> sessionManager = new HashMap<>();
 
     // member id, message
-    private final Map<Long, String> messageOffer = new HashMap<>();
+    private final Map<String, String> messageOffer = new HashMap<>();
 
     public void startLive(WebSocket socket, LiveRequestDto messageObj) {
-        Lecture lectureToStart = em.find(Lecture.class, messageObj.lectureId);
-        ValidatePermission.validateLecturer(messageObj.userId, lectureToStart);
+        Lecture lecture = em.find(Lecture.class, messageObj.lectureId);
+        ValidatePermission.validateLecturer(messageObj.userId, lecture);
         startLiveLecture(messageObj.lectureId, messageObj.userId, socket);
 
         socket.send("라이브 생성 성공");
@@ -68,19 +75,20 @@ public class WebSocketService {
     }
 
     public void offer(WebSocket socket, LiveRequestDto messageObj, String message) {
-            // 본인 sdp를 포함한 메세지 저장
-            messageOffer.put(messageObj.userId, message);
+        // 본인 sdp를 포함한 메세지 저장
+        String encryptedUser = convertedToEncryption(messageObj.lectureId, messageObj.userId);
+        messageOffer.put(encryptedUser, message);
 
-            // 본인을 제외한 나머지 참여자에게 offer 전달
-            sendToAll(messageObj.lectureId, messageObj.userId,  message);
+        // 본인을 제외한 나머지 참여자에게 offer 전달
+        sendToAll(messageObj.lectureId, messageObj.userId,  message);
 
-            // 나머지 참여자의 offer 본인에게 전달
-            for (Long sessionMember : sessionManager.get(messageObj.lectureId)) {
-                // 본인을 제외한 모두에게 요청보냄
-                if (!sessionMember.equals(messageObj.userId)) {
-                    socket.send(messageOffer.get(sessionMember));
-                }
+        // 나머지 참여자의 offer 본인에게 전달
+        for (String target : sessionManager.get(changeLongToString(messageObj.lectureId))) {
+            // 본인을 제외한 모두에게 요청보냄
+            if (!target.equals(encryptedUser)) {
+                socket.send(messageOffer.get(target));
             }
+        }
     }
 
     public void answer(WebSocket socket, LiveRequestDto messageObj, String message) {
@@ -90,62 +98,71 @@ public class WebSocketService {
 
         // Json 문자열 -> Map
         Map<String, Object> map = gson.fromJson(message, Map.class);
-        if(map.containsKey("sender") && map.get("sender") instanceof Long
-                && connections.containsKey((Long)map.get("sender"))) connections.get((Long)map.get("sender")).send(message);
+        if(map.containsKey("sender") && map.get("sender") instanceof Long) {
+            String encryptedSender = convertedToEncryption(messageObj.lectureId, (Long) map.get("sender"));
+            if(connections.containsKey(encryptedSender)) connections.get(encryptedSender).send(message);
+        }
         else {
             sendToAll(messageObj.lectureId, messageObj.userId, message);
         }
     }
 
     public void exitLive(WebSocket socket, LiveRequestDto messageObj) {
-        Member member = em.find(Member.class, messageObj.userId);
         Lecture lecture = em.find(Lecture.class, messageObj.lectureId);
 
         // TODO: 잘못된 사용자 요청 검증
         // if(member == null || lecture == null)
 
         // 강사의 강의 종료
-        if(lecture.getId().equals(messageObj.userId)) {
+        if(lecture.getLecturer().getId().equals(messageObj.userId)) {
             // sessionManager 돌면서 현재 session에 참여하고 있는 user 탐색
-            for (Long needToRemove : sessionManager.get(lecture.getId())) {
+            String lectureToString = changeLongToString(lecture.getId());
+            for (String needToRemove : sessionManager.get(lectureToString)) {
                 removeConnections(needToRemove);
             }
-            sessionManager.remove(lecture.getId());
+            sessionManager.remove(lectureToString);
         }
         // 수강생 강의 종료
         else {
-             removeConnections(messageObj.userId);
-             sessionManager.get(lecture.getId()).remove(messageObj.userId);
+             String encryptedUser = convertedToEncryption(messageObj.lectureId, messageObj.userId);
+             removeConnections(encryptedUser);
+             sessionManager.get(changeLongToString(lecture.getId())).remove(encryptedUser);
              sendToAll(messageObj.lectureId, messageObj.userId, String.format("%s 님이 라이브 강의를 나갔습니다.", messageObj.userId));
         }
         log.info("client exited: {}", messageObj.userId);
     }
 
 
-    private void sendToAll(Long lectureId, Long requester, String message) {
-        for (Long member : sessionManager.get(lectureId)) {
+    private void sendToAll(Long lectureId, Long userId, String message) {
+        String changedValue = convertedToEncryption(lectureId, userId);
+        for (String target : sessionManager.get(changeLongToString(lectureId))) {
             // 본인을 제외한 모두에게 요청보냄
-            if (!member.equals(requester)) connections.get(member).send(message);
+            if (!target.equals(changedValue)) connections.get(target).send(message);
         }
     }
 
-    private void startLiveLecture(Long lectureId, Long lecturerId, WebSocket conn) {
-        sessionManager.put(lectureId, new LinkedList<>());
-        sessionManager.get(lectureId).add(lecturerId);
+    private void startLiveLecture(Long lectureId, Long userId, WebSocket conn) {
+        String lectureToString = changeLongToString(lectureId);
+        sessionManager.put(lectureToString, new LinkedList<>());
+        String changedValue = convertedToEncryption(lectureId, userId);
+        sessionManager.get(lectureToString).add(changedValue);
+        connections.put(changedValue, conn);
 
-        connections.put(lecturerId, conn);
         log.info("강의 세션이 생성되었습니다.");
     }
 
-    private void enterLiveLecture(Long lectureId, Long studentId, WebSocket conn) {
-        if(!sessionManager.containsKey(lectureId)) throw new IllegalArgumentException("현재 진행하지 않는 강의입니다.");
+    private void enterLiveLecture(Long lectureId, Long userId, WebSocket conn) {
+        String lectureToString = changeLongToString(lectureId);
+        if(!sessionManager.containsKey(lectureToString)) throw new IllegalArgumentException("현재 진행하지 않는 강의입니다.");
 
-        sessionManager.get(lectureId).add(studentId);
-        connections.put(studentId, conn);
+        String changedValue = convertedToEncryption(lectureId, userId);
+        sessionManager.get(lectureToString).add(changedValue);
+        connections.put(changedValue, conn);
+
         log.info("강의 세션에 참가하였습니다");
     }
 
-    private void removeConnections(Long target) {
+    private void removeConnections(String target) {
         if(connections.containsKey(target)){
             connections.get(target).close();
             connections.remove(target);
