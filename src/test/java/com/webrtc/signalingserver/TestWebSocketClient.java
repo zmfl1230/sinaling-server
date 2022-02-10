61943dc4ea7a7c8954c3d9080138fb4742753067
zmfl1230 <zmfl123097@gmail.com>
2022-02-10 22:54:29 +0900

    feat: [서비스 로직 테스트 코드] web socket client 생성 for test

---
+++ src/test/java/com/webrtc/signalingserver/TestWebSocketClient.java
@@ -0,0 +1,42 @@
+package com.webrtc.signalingserver;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import javax.websocket.*;
+import java.net.URI;
+
+@ClientEndpoint
+public class TestWebSocketClient extends WebSocketClient {
+
+    public TestWebSocketClient(URI endpointURI) {
+        super(endpointURI);
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            container.connectToServer(this, endpointURI);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        System.out.println("handshakedata.getHttpStatus() = " + handshakedata.getHttpStatus());
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("message = " + message);
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("reason = " + reason);
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        System.out.println("ex.getMessage() = " + ex.getMessage());
+    }
+
+}
