c3e4bab352e27c50699b31d012cd9f3f02f524f3
zmfl1230 <zmfl123097@gmail.com>
2022-02-14 23:39:36 +0900

    feat: [Constants] 재사용 가능하도록 상수 정의

---
+++ src/main/java/com/webrtc/signalingserver/Constants.java
@@ -0,0 +1,7 @@
+package com.webrtc.signalingserver;
+
+public class Constants {
+    public static final String DELIMITER = "-";
+    public static final int LECTURE = 0;
+    public static final int MEMBER = 1;
+}
