282381e31e1273d7387956d793ea57ce839ce591
zmfl1230 <zmfl123097@gmail.com>
2022-02-22 21:49:03 +0900

    refactor: [EnterLiveTest] 공통 요청 함수 모듈화 반영

--- src/test/java/com/webrtc/signalingserver/service/EnterLiveTest.java
+++ src/test/java/com/webrtc/signalingserver/service/EnterLiveTest.java
@@ -1,7 +1,5 @@
 package com.webrtc.signalingserver.service;
 
-import com.webrtc.signalingserver.WebSocketClientStub;
-import com.webrtc.signalingserver.domain.dto.LiveRequestDto;
 import com.webrtc.signalingserver.domain.entity.Lecture;
 import com.webrtc.signalingserver.domain.entity.Member;
 import com.webrtc.signalingserver.domain.entity.MemberRole;
@@ -9,24 +7,18 @@ import com.webrtc.signalingserver.repository.MemoryRepository;
 import com.webrtc.signalingserver.repository.MemorySessionRepository;
 import com.webrtc.signalingserver.repository.ObjectRepository;
 import com.webrtc.signalingserver.repository.SessionRepository;
-import org.java_websocket.client.WebSocketClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.net.URI;
-
 import static com.webrtc.signalingserver.util.EncryptString.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnterLiveTest {
-    WebSocketClient teacherClient;
-    WebSocketClient studentClient;
     ObjectRepository objectRepository;
     SessionRepository sessionRepository;
-    WebSocketService webSocketService;
-    TemplateForSynchronized template;
+    CommonRequest commonRequest;
 
     Member teacher;
     Member student1;
@@ -39,42 +31,38 @@ public class EnterLiveTest {
     public void setUp() {
         objectRepository = new MemoryRepository();
         sessionRepository = new MemorySessionRepository();
-        template = new TemplateForSynchronized();
-        webSocketService = new WebSocketService(objectRepository, sessionRepository, template);
+
+        commonRequest = new CommonRequest(objectRepository, sessionRepository);
 
         teacher = new Member(1L, "teacher", MemberRole.LECTURER);
         student1 = new Member(2L, "student1", MemberRole.STUDENT);
         student2 = new Member(3L, "student2", MemberRole.STUDENT);
 
         lecture = new Lecture(1L, teacher);
-        lecture.getStudents().add(student1);
         lecture1 = new Lecture(2L, teacher);
+        lecture.getStudents().add(student1);
 
         objectRepository.saveMember(teacher);
         objectRepository.saveMember(student1);
+        objectRepository.saveMember(student2);
         objectRepository.saveLecture(lecture);
+        objectRepository.saveLecture(lecture1);
 
         // start live
-        teacherClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-        LiveRequestDto startLive = LiveRequestDto.buildBasicDto("startLive", teacher.getId(), lecture.getId(), null);
-        webSocketService.startLive(teacherClient.getConnection(), startLive);
+        commonRequest.startLive(teacher.getId(), lecture.getId());
     }
 
     @Test
     @DisplayName("강의 수강자 접속 시, LectureSession 및 Connection 에 저장 여부 확인")
     public void enterLiveAuthorizedStudent(){
-        //Given
-        LiveRequestDto enterLive = LiveRequestDto.buildBasicDto("enterLive", student1.getId(), lecture.getId(), null);
-
         //When
-        studentClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-        webSocketService.enterLive(studentClient.getConnection(), enterLive);
+        commonRequest.enterLive(student1.getId(), lecture.getId());
 
         //Then
         String encryptedKey = changeLongToString(lecture.getId(), student1.getId());
         //connection 저장 여부 확인
         assertThat(sessionRepository.containsKeyOnConnections(encryptedKey)).isTrue();
-        assertThat(sessionRepository.getWebSocketOnConnections(encryptedKey)).isSameAs(studentClient.getConnection());
+        assertThat(sessionRepository.getWebSocketOnConnections(encryptedKey)).isSameAs(commonRequest.getConnection());
 
         // 해당 lecture session 에 학생 키 저장되어 있는지 확인
         assertThat(sessionRepository.getConnectionsByLectureId(changeLongToString(lecture.getId())).contains(encryptedKey)).isTrue();
@@ -84,28 +72,14 @@ public class EnterLiveTest {
     @Test
     @DisplayName("강의 수강생이 아닌 학생이 접속 요청 시, exception")
     public void enterLiveUnAuthorizedStudent(){
-        //Given
-        objectRepository.saveMember(student2);
-        LiveRequestDto enterLive = LiveRequestDto.buildBasicDto("enterLive", student2.getId(), lecture.getId(), null);
-
-        //When
-        studentClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-
-        //Then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> webSocketService.enterLive(studentClient.getConnection(), enterLive));
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commonRequest.enterLive(student2.getId(), lecture.getId()));
     }
 
     @Test
     @DisplayName("lecture session에 없는 라이브 강의에 접속 요청 시, exception")
     public void enterLectureNotExisted(){
-        //Given
-        objectRepository.saveLecture(lecture1);
-        LiveRequestDto enterLive = LiveRequestDto.buildBasicDto("enterLive", student1.getId(), lecture1.getId(), null);
-
-        //When
-        studentClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-
-        //Then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> webSocketService.enterLive(studentClient.getConnection(), enterLive));
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commonRequest.enterLive(student1.getId(), lecture1.getId()));
     }
 }
