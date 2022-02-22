66368e3d7683fa86570b1e67e7d7129a9359a63c
zmfl1230 <zmfl123097@gmail.com>
2022-02-22 22:11:15 +0900

    refactor: [ExitLiveTest] 공통 요청 함수 모듈화 반영

--- src/test/java/com/webrtc/signalingserver/service/ExitLiveTest.java
+++ src/test/java/com/webrtc/signalingserver/service/ExitLiveTest.java
@@ -1,7 +1,5 @@
 package com.webrtc.signalingserver.service;
 
-import com.webrtc.signalingserver.WebSocketClientStub;
-import com.webrtc.signalingserver.domain.dto.LiveRequestDto;
 import com.webrtc.signalingserver.domain.entity.Lecture;
 import com.webrtc.signalingserver.domain.entity.Member;
 import com.webrtc.signalingserver.domain.entity.MemberRole;
@@ -9,24 +7,19 @@ import com.webrtc.signalingserver.repository.MemoryRepository;
 import com.webrtc.signalingserver.repository.MemorySessionRepository;
 import com.webrtc.signalingserver.repository.ObjectRepository;
 import com.webrtc.signalingserver.repository.SessionRepository;
-import org.java_websocket.client.WebSocketClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.net.URI;
 
 import static com.webrtc.signalingserver.util.EncryptString.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExitLiveTest {
 
-    WebSocketClient teacherClient;
-    TemplateForSynchronized template;
     ObjectRepository objectRepository;
     SessionRepository sessionRepository;
-    WebSocketService webSocketService;
+    CommonRequest commonRequest;
 
     Member teacher1;
     Member teacher2;
@@ -40,11 +33,11 @@ public class ExitLiveTest {
     public void setUp() {
         objectRepository = new MemoryRepository();
         sessionRepository = new MemorySessionRepository();
-        template = new TemplateForSynchronized();
-        webSocketService = new WebSocketService(objectRepository, sessionRepository, template);
 
-        teacher1 = new Member(1L, "teacher", MemberRole.LECTURER);
-        teacher2 = new Member(4L, "teacher1", MemberRole.LECTURER);
+        commonRequest = new CommonRequest(objectRepository, sessionRepository);
+
+        teacher1 = new Member(1L, "teacher1", MemberRole.LECTURER);
+        teacher2 = new Member(4L, "teacher2", MemberRole.LECTURER);
         student1 = new Member(2L, "student1", MemberRole.STUDENT);
         student2 = new Member(3L, "student2", MemberRole.STUDENT);
 
@@ -61,14 +54,12 @@ public class ExitLiveTest {
         objectRepository.saveLecture(lecture);
         objectRepository.saveLecture(lecture1);
 
-        // start live
-        teacherClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-        LiveRequestDto startLive1 = LiveRequestDto.buildBasicDto("startLive", teacher1.getId(), lecture.getId(), null);
-        webSocketService.startLive(teacherClient.getConnection(), startLive1);
+        // start live - lecture
+        commonRequest.startLive(teacher1.getId(), lecture.getId());
 
-        teacherClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-        LiveRequestDto startLive2 = LiveRequestDto.buildBasicDto("startLive", teacher1.getId(), lecture1.getId(), null);
-        webSocketService.startLive(teacherClient.getConnection(), startLive2);
+        // start live - lecture1
+        commonRequest.startLive(teacher1.getId(), lecture1.getId());
+
     }
 
     // 1. 강의자 강의 세션 종료
@@ -79,13 +70,11 @@ public class ExitLiveTest {
 
         //given
         for (Member student : lecture.getStudents()) {
-            LiveRequestDto enterLive = LiveRequestDto.buildBasicDto("enterLive", student.getId(), lecture.getId(), null);
-            webSocketService.enterLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), enterLive);
+            commonRequest.enterLive(student.getId(), lecture.getId());
         }
 
         //when
-        LiveRequestDto exitLive = LiveRequestDto.buildBasicDto("exitLive", lecture.getLecturer().getId(), lecture.getId(), null);
-        webSocketService.exitLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), exitLive);
+        commonRequest.exitLive(lecture.getLecturer().getId(), lecture.getId());
 
         //then
         // lectureSession에서 해당 강의 제거되었는지 확인
@@ -108,15 +97,12 @@ public class ExitLiveTest {
 
         //given
         for (Member student : lecture.getStudents()) {
-            LiveRequestDto enterLive = LiveRequestDto.buildBasicDto("enterLive", student.getId(), lecture.getId(), null);
-            webSocketService.enterLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), enterLive);
+            commonRequest.enterLive(student.getId(), lecture.getId());
         }
 
-        //when
-        LiveRequestDto exitLive = LiveRequestDto.buildBasicDto("exitLive", teacher2.getId(), lecture.getId(), null);
-
         //then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> webSocketService.exitLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), exitLive) );
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commonRequest.exitLive(teacher2.getId(), lecture.getId()));
 
     }
 
@@ -126,25 +112,14 @@ public class ExitLiveTest {
 
         //given
         for (Member student : lecture1.getStudents()) {
-            LiveRequestDto enterLive = LiveRequestDto.buildBasicDto("enterLive", student.getId(), lecture1.getId(), null);
-            webSocketService.enterLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), enterLive);
+            commonRequest.enterLive(student.getId(), lecture1.getId());
         }
 
         //when
-        LiveRequestDto exitLive = LiveRequestDto.buildBasicDto("exitLive", teacher2.getId(), lecture1.getId(), null);
-        webSocketService.exitLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), exitLive);
+        commonRequest.exitLive(teacher2.getId(), lecture1.getId());
 
         //then
-        // lectureSession에서 해당 강의가 아직 존재하는지 확인(삭제되면 안됨)
-        assertThat(sessionRepository.containsLectureSessionOnSessionManager(changeLongToString(lecture1.getId()))).isTrue();
-
-        // 강의자 관련 데이터가 아직 존재하는지 확인(삭제되면 안됨)
-        String encryption = changeLongToString(lecture1.getId(), lecture1.getLecturer().getId());
-        assertThat(sessionRepository.containsKeyOnConnections(encryption)).isTrue();
-
-        // 강의 수강생 관련 데이터가 완전히 제거되었는지 확인
-        encryption = changeLongToString(lecture1.getId(), teacher2.getId());
-        assertThat(sessionRepository.containsKeyOnConnections(encryption)).isFalse();
+        validateResult(lecture1);
 
     }
     // 2. 학생 강의 종료
@@ -155,15 +130,17 @@ public class ExitLiveTest {
 
         //given
         for (Member student : lecture.getStudents()) {
-            LiveRequestDto enterLive = LiveRequestDto.buildBasicDto("enterLive", student.getId(), lecture.getId(), null);
-            webSocketService.enterLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), enterLive);
+            commonRequest.enterLive(student.getId(), lecture.getId());
         }
-
         //when
-        LiveRequestDto exitLive = LiveRequestDto.buildBasicDto("exitLive", student1.getId(), lecture.getId(), null);
-        webSocketService.exitLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), exitLive);
+        commonRequest.exitLive(student1.getId(), lecture.getId());
 
         //then
+        validateResult(lecture);
+    }
+
+    private void validateResult(Lecture lecture) {
+
         // lectureSession에서 해당 강의가 아직 존재하는지 확인(삭제되면 안됨)
         assertThat(sessionRepository.containsLectureSessionOnSessionManager(changeLongToString(lecture.getId()))).isTrue();
 
@@ -174,29 +151,20 @@ public class ExitLiveTest {
         // 강의 수강생 관련 데이터가 완전히 제거되었는지 확인
         encryption = changeLongToString(lecture.getId(), student1.getId());
         assertThat(sessionRepository.containsKeyOnConnections(encryption)).isFalse();
+
     }
 
     @Test
     @DisplayName("권한이 없는 학생이 강의 종료 시, exception")
     public void leaveLiveByUnUnAuthorizedStudent() {
-
-        //given
-        LiveRequestDto exitLive = LiveRequestDto.buildBasicDto("exitLive", student1.getId(), lecture1.getId(), null);
-
-        //then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> webSocketService.exitLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), exitLive) );
-
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commonRequest.exitLive(student1.getId(), lecture1.getId()));
     }
 
     @Test
     @DisplayName("세션에 없는 학생이 강의 종료 시, exception")
     public void exitLiveByUnAuthorizedStudent() {
-
-        //given
-        LiveRequestDto exitLive = LiveRequestDto.buildBasicDto("exitLive", student1.getId(), lecture1.getId(), null);
-
-        //then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> webSocketService.exitLive(new WebSocketClientStub(URI.create("ws://localhost:8888/")).getConnection(), exitLive) );
-
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commonRequest.exitLive(student1.getId(), lecture1.getId()));
     }
 }
