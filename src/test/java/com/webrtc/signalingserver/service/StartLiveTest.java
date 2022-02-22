af9e903016868fa00c1e60d18e2c4eb795da50ab
zmfl1230 <zmfl123097@gmail.com>
2022-02-22 21:42:47 +0900

    refactor: [StartLiveTest] 공통 요청 함수 모듈화 반영

--- src/test/java/com/webrtc/signalingserver/service/StartLiveTest.java
+++ src/test/java/com/webrtc/signalingserver/service/StartLiveTest.java
@@ -1,7 +1,5 @@
 package com.webrtc.signalingserver.service;
 
-import com.webrtc.signalingserver.WebSocketClientStub;
-import com.webrtc.signalingserver.domain.dto.LiveRequestDto;
 import com.webrtc.signalingserver.domain.entity.Lecture;
 import com.webrtc.signalingserver.domain.entity.Member;
 import com.webrtc.signalingserver.domain.entity.MemberRole;
@@ -9,25 +7,19 @@ import com.webrtc.signalingserver.repository.MemoryRepository;
 import com.webrtc.signalingserver.repository.MemorySessionRepository;
 import com.webrtc.signalingserver.repository.ObjectRepository;
 import com.webrtc.signalingserver.repository.SessionRepository;
-import org.java_websocket.client.WebSocketClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.net.URI;
 
 import static com.webrtc.signalingserver.util.EncryptString.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class StartLiveTest {
 
-
-    WebSocketClient client;
     ObjectRepository objectRepository;
     SessionRepository sessionRepository;
-    WebSocketService webSocketService;
-    TemplateForSynchronized template;
+    CommonRequest commonRequest;
 
     Member teacher;
     Member teacher1;
@@ -38,12 +30,10 @@ public class StartLiveTest {
 
     @BeforeEach
     public void setUpObject() {
-        client = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-
         objectRepository = new MemoryRepository();
         sessionRepository = new MemorySessionRepository();
-        template = new TemplateForSynchronized();
-        webSocketService = new WebSocketService(objectRepository, sessionRepository, template);
+
+        commonRequest = new CommonRequest(objectRepository, sessionRepository);
 
         teacher = new Member(1L, "teacher", MemberRole.LECTURER);
         teacher1 = new Member(4L, "teacher1", MemberRole.LECTURER);
@@ -52,22 +42,25 @@ public class StartLiveTest {
         lecture = new Lecture(1L, teacher);
         lecture1 = new Lecture(2L, teacher1);
         lecture1.getStudents().add(student1);
-    }
 
+        objectRepository.saveMember(teacher);
+        objectRepository.saveMember(teacher1);
+        objectRepository.saveMember(student1);
+        objectRepository.saveLecture(lecture);
+        objectRepository.saveLecture(lecture1);
+    }
 
 
     @Test
     @DisplayName("강의자가 본인 강의 라이브 시작")
     public void startLiveByCorrectLecturer() {
-
-        //given
-        objectRepository.saveMember(teacher);
-        objectRepository.saveLecture(lecture);
-
-        LiveRequestDto startLive = LiveRequestDto.buildBasicDto("startLive", teacher.getId(), lecture.getId(), null);
-
         //when
-        webSocketService.startLive(client.getConnection(), startLive);
+        commonRequest.startLive(teacher.getId(), lecture.getId());
+
+        //then
+        assertThat(sessionRepository.containsLectureSessionOnSessionManager(changeLongToString(lecture.getId())))
+                .isTrue();
+        assertThat(sessionRepository.getConnectionsByLectureId(changeLongToString(lecture.getId())).size()).isEqualTo(1);
 
     }
 
@@ -75,48 +68,26 @@ public class StartLiveTest {
     @Test
     @DisplayName("강의자가 본인강의가 아닌 강의 라이브 시작 -> IllegalArgumentException")
     public void startLiveByWrongLecturer() {
-
-        //Given
-        objectRepository.saveMember(teacher);
-        objectRepository.saveMember(teacher1);
-        objectRepository.saveLecture(lecture);
-
-        LiveRequestDto startLive = LiveRequestDto.buildBasicDto("startLive", teacher1.getId(), lecture.getId(), null);
-
-        //Then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> webSocketService.startLive(client.getConnection(), startLive));
-
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commonRequest.startLive(teacher1.getId(), lecture.getId()));
     }
 
     @Test
     @DisplayName("강의자가 아닌 학생이 라이브 시작 -> IllegalArgumentException")
     public void startLiveByStudent() {
-
-        //Given
-        objectRepository.saveMember(teacher);
-        objectRepository.saveMember(student1);
-        objectRepository.saveLecture(lecture);
-
-        LiveRequestDto startLive = LiveRequestDto.buildBasicDto("startLive", teacher1.getId(), lecture.getId(), null);
-        //Then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> webSocketService.startLive(client.getConnection(), startLive));
-
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commonRequest.startLive(teacher1.getId(), lecture.getId()));
     }
 
     @Test
     @DisplayName("라이브 강의 생성시, sessionManager 생성 확인")
     public void checkSessionManager() {
-        //given
-        objectRepository.saveMember(teacher);
-        objectRepository.saveLecture(lecture);
-
-        LiveRequestDto startLive = LiveRequestDto.buildBasicDto("startLive", teacher.getId(), lecture.getId(), null);
         //when
-        webSocketService.startLive(client.getConnection(), startLive);
+        commonRequest.startLive(teacher.getId(), lecture.getId());
 
         //given
         String encryptedKey = changeLongToString(teacher.getId(), lecture.getId());
         assertThat(sessionRepository.containsKeyOnConnections(encryptedKey)).isTrue();
-        assertThat(sessionRepository.getWebSocketOnConnections(encryptedKey)).isSameAs(client.getConnection());
+        assertThat(sessionRepository.getWebSocketOnConnections(encryptedKey)).isSameAs(commonRequest.getConnection());
     }
 }
