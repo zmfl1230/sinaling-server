2f26539e2be775b0e377ba12a64e2944580423a4
zmfl1230 <zmfl123097@gmail.com>
2022-02-22 22:00:15 +0900

    refactor: [EnterWaitingRoomTest] 공통 요청 함수 모듈화 반영

--- src/test/java/com/webrtc/signalingserver/service/EnterWaitingRoomTest.java
+++ src/test/java/com/webrtc/signalingserver/service/EnterWaitingRoomTest.java
@@ -1,7 +1,5 @@
 package com.webrtc.signalingserver.service;
 
-import com.webrtc.signalingserver.WebSocketClientStub;
-import com.webrtc.signalingserver.domain.dto.LiveRequestDto;
 import com.webrtc.signalingserver.domain.entity.Lecture;
 import com.webrtc.signalingserver.domain.entity.Member;
 import com.webrtc.signalingserver.domain.entity.MemberRole;
@@ -9,12 +7,10 @@ import com.webrtc.signalingserver.repository.MemoryRepository;
 import com.webrtc.signalingserver.repository.MemorySessionRepository;
 import com.webrtc.signalingserver.repository.ObjectRepository;
 import com.webrtc.signalingserver.repository.SessionRepository;
-import org.java_websocket.client.WebSocketClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.net.URI;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,53 +19,43 @@ import static com.webrtc.signalingserver.util.EncryptString.changeLongToString;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class EnterWaitingRoomTest {
-    WebSocketClient client;
-    WebSocketClient client1;
-    WebSocketClient teacherClient;
+
     ObjectRepository objectRepository;
     SessionRepository sessionRepository;
-    WebSocketService webSocketService;
-    TemplateForSynchronized template;
+    CommonRequest commonRequest;
 
     Member teacher;
-    Member student;
     Member student1;
+    Member student2;
+
     Lecture lecture;
 
     @BeforeEach
-    public void setUp() {
+    public void setUpObject() {
         objectRepository = new MemoryRepository();
         sessionRepository = new MemorySessionRepository();
-        template = new TemplateForSynchronized();
-        webSocketService = new WebSocketService(objectRepository, sessionRepository, template);
+
+        commonRequest = new CommonRequest(objectRepository, sessionRepository);
 
         teacher = new Member(1L, "teacher", MemberRole.LECTURER);
-        student = new Member(2L, "student1", MemberRole.STUDENT);
-        student1 = new Member(3L, "student1", MemberRole.STUDENT);
+        student1 = new Member(2L, "student1", MemberRole.STUDENT);
+        student2 = new Member(3L, "student2", MemberRole.STUDENT);
 
         lecture = new Lecture(1L, teacher);
-        lecture.getStudents().add(student);
         lecture.getStudents().add(student1);
+        lecture.getStudents().add(student2);
 
         objectRepository.saveMember(teacher);
-        objectRepository.saveMember(student);
         objectRepository.saveMember(student1);
+        objectRepository.saveMember(student2);
         objectRepository.saveLecture(lecture);
-
     }
 
     @Test
     @DisplayName("대기실에 입장 시도(강의 생성 이전), 정상적으로 waiting room에 컨넥션 적재 성공")
-    public void enterWaitingRoomBeforeLiveStarted() throws Exception {
-        //Given
-        LiveRequestDto enterWaitingRoom = LiveRequestDto.buildBasicDto("enterWaitingRoom", student.getId(), lecture.getId(), null);
-        client = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-
-        LiveRequestDto enterWaitingRoom1 = LiveRequestDto.buildBasicDto("enterWaitingRoom", student1.getId(), lecture.getId(), null);
-        client1 = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-
+    public void enterWaitingRoomBeforeLiveStarted() {
         //When
-        webSocketService.enterWaitingRoom(client.getConnection(), enterWaitingRoom);
+        commonRequest.enterWaitingRoom(student1.getId(), lecture.getId());
 
         //Then
         // 정상적으로 대기실이 생성되는지 확인
@@ -78,7 +64,7 @@ class EnterWaitingRoomTest {
         assertThat(sessionRepository.getConnectionsOnWaitingRoom(changeLongToString(lecture.getId())).size()).isEqualTo(1);
 
         //When
-        webSocketService.enterWaitingRoom(client1.getConnection(), enterWaitingRoom1);
+        commonRequest.enterWaitingRoom(student1.getId(), lecture.getId());
 
         //Then
         // 두번쨰 대기실 입장 요청시 클라이언트가 정상적으로 담기는지 확인
@@ -88,20 +74,15 @@ class EnterWaitingRoomTest {
 
     @Test
     @DisplayName("대기실 입장 시도(강의 생성 이후) 대기실 입장 거부, 생성 X")
-    public void enterWaitingRoomAfterLiveStarted() throws Exception {
+    public void enterWaitingRoomAfterLiveStarted() {
 
         //Given
         // start live
-        teacherClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-        LiveRequestDto startLive = LiveRequestDto.buildBasicDto("startLive", teacher.getId(), lecture.getId(), null);
-        webSocketService.startLive(teacherClient.getConnection(), startLive);
-
-        // 강의 생성 대기실 접속 요청
-        LiveRequestDto enterWaitingRoom = LiveRequestDto.buildBasicDto("enterWaitingRoom", student.getId(), lecture.getId(), null);
-        client = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
+        commonRequest.startLive(teacher.getId(), lecture.getId());
 
         //When
-        webSocketService.enterWaitingRoom(client.getConnection(), enterWaitingRoom);
+        // 강의 생성 후, 대기실 접속 요청
+        commonRequest.enterWaitingRoom(student1.getId(), lecture.getId());
 
         //Then
         // 정상적으로 대기실이 생성이 거부되었는지 확인
@@ -119,27 +100,27 @@ class EnterWaitingRoomTest {
         CountDownLatch latch = new CountDownLatch(numberOfThreads);
 
         service.submit(() -> {
-            enterWaitingRoom(student.getId());
+            commonRequest.enterWaitingRoom(student1.getId(), lecture.getId());
             latch.countDown();
         });
         service.submit(() -> {
-            enterWaitingRoom(student.getId());
+            commonRequest.enterWaitingRoom(student2.getId(), lecture.getId());
             latch.countDown();
         });
         service.submit(() -> {
-            enterWaitingRoom(student.getId());
+            commonRequest.enterWaitingRoom(student1.getId(), lecture.getId());
             latch.countDown();
         });
         service.submit(() -> {
-            enterWaitingRoom(student.getId());
+            commonRequest.enterWaitingRoom(student2.getId(), lecture.getId());
             latch.countDown();
         });
         service.submit(() -> {
-            enterWaitingRoom(student.getId());
+            commonRequest.enterWaitingRoom(student1.getId(), lecture.getId());
             latch.countDown();
         });
         service.submit(() -> {
-            createLectureLive();
+            commonRequest.startLive(teacher.getId(), lecture.getId());
             latch.countDown();
         });
 
@@ -148,18 +129,6 @@ class EnterWaitingRoomTest {
         //Then
         assertThat(sessionRepository.containsKeyOnWaitingRoom(changeLongToString(lecture.getId()))).isFalse();
 
-    }
-
-    void createLectureLive() {
-        teacherClient = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-        LiveRequestDto startLive = LiveRequestDto.buildBasicDto("startLive", teacher.getId(), lecture.getId(), null);
-        webSocketService.startLive(teacherClient.getConnection(), startLive);
-    }
-
-    void enterWaitingRoom(Long studentId) {
-        LiveRequestDto enterWaitingRoom = LiveRequestDto.buildBasicDto("enterWaitingRoom", studentId, lecture.getId(), null);
-        client = new WebSocketClientStub(URI.create("ws://localhost:8888/"));
-        webSocketService.enterWaitingRoom(client.getConnection(), enterWaitingRoom);
     }
 
 }
